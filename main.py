4481c116f5591054d2213d20acbb6a27b89a5ce8
eduardo.diez <eduardo.diez@coinscrapfinance.com>
2024-06-27 10:57:11 +0200

    [UPDATE] use env var

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ import logging.config
 import logging.handlers
 import pathlib
 
+from envyaml import EnvYAML
+
 logger = logging.getLogger("my_app")  # __name__ is a common choice
 
 
 def setup_logging():
-    config_file = pathlib.Path("logging_configs/2-stderr-json-file.yaml")
-    with open(config_file) as f_in:
-        config = yaml.safe_load(f_in)
-        # config = json.load(f_in)
+    env = EnvYAML('logging_configs/3-homework-filtered-stdout-stderr.yaml')
+    
+    # config_file = pathlib.Path("logging_configs/3-homework-filtered-stdout-stderr.yaml")
+    # with open(file=config_file, mode="r", encoding="utf-8") as f_in:
+    #     config = yaml.safe_load(f_in)
+    #     # config = json.load(f_in)
+
+    config = env.__dict__['_EnvYAML__cfg']
 
     logging.config.dictConfig(config)
-    # queue_handler = logging.getHandlerByName("queue_handler")
-    # if queue_handler is not None:
-    #     queue_handler.listener.start()
-    #     atexit.register(queue_handler.listener.stop)
+    # # queue_handler = logging.getHandlerByName("queue_handler")
+    # # if queue_handler is not None:
+    # #     queue_handler.listener.start()
+    # #     atexit.register(queue_handler.listener.stop)
 
 
 def main():
